caac0f888e71652c46efeed291b3a7dcd04a67b4
nthduc95 <nthduc95@gmail.com>
2023-08-10 10:58:55 +0700

    config enviroment dotenv

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,3 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config({
+  path: `.env`,
+});
+
 export const config = {
   'username': process.env.POSTGRES_USERNAME,
   'password': process.env.POSTGRES_PASSWORD,
