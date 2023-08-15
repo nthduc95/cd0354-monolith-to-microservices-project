0d27e4f46f572d4221535b2e595e10e94e5516c6
Đức Nguyễn <>
2023-08-16 00:17:07 +0700

    change api config front end

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8f23b31772174fdc8e09e5c2e7cc0ff-139190783.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
